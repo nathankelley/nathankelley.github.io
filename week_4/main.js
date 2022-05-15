b32226ec8c048a2145a13e14c6ee2b38c4eb95b6
Nate <kel18018@byui.edu>
2022-05-14 20:11:15 -0700

    Notes on javascript forms and practice programs

--- week_4/main.js
+++ week_4/main.js
@@ -17,7 +17,60 @@ input.addEventListener('change', () => alert('changed'), false);
 // submit button - current action
 form.addEventListener ('submit', search, false);
 
-function search() {
-    alert(' Form Submitted');
+function search(event) {
+    alert(`You submitted: ${input.value}`);
     event.preventDefault(); // stops form from being submitted altogether
+}
+
+// input field auto-value
+input.value = 'Submit Here';
+
+// this will take the text away when user clicks in box
+input.addEventListener('focus', function(){
+    if(input.value==='Submit Here'){
+        input.value = '';
+    } 
+}, false);
+// this will add the text if the user leaves the box without text
+input.addEventListener('blur', function(){
+    if(input.value===''){
+        input.value = 'Submit Here';
+    } 
+}, false);
+
+
+
+// FOR SUPERHERO FORM
+const form_2 = document.forms['hero'];
+form_2.addEventListener('submit', makeHero, false);
+
+// create hero object
+function makeHero(event) {
+    event.preventDefault();
+    const hero = {};
+    hero.name = form_2.heroName.value;
+
+    // set hero secret name
+    hero.realName = form_2.realName.value;
+
+    // get hero powers
+    hero.powers = [];
+    for (let i = 0; i < form_2.powers.length; i++) {
+        if (form_2.powers[i].checked){
+            hero.powers.push(form_2.powers[i].value);
+            }
+    }
+
+    // get hero type
+    hero.category = form_2.category.value;
+
+    // get base of operations
+    hero.city = form_2.city.value;
+
+    // get origin story
+    hero.origin = form_2.origin.value;
+
+    // this method in JSON will convert hero object to string to display
+    alert(JSON.stringify(hero)); 
+    return hero;
 }
